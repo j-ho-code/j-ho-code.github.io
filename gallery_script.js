34863c969174727b2b723b6ca969fcd1243c36ef
john <john@seanho.com>
2025-03-01 11:38:03 -0800

    Added 4 images in gallery script

--- gallery_script.js
+++ gallery_script.js
@@ -1,3 +1,6 @@
-const imageGallery = {
-    ""
-}
+const imageGallery = [
+    "https://cdn.pixabay.com/photo/2015/11/01/19/47/minecraft-1017471_1280.jpg",
+    "https://cdn.pixabay.com/photo/2015/03/02/12/11/minecraft-655908_1280.png",
+    "https://cdn.pixabay.com/photo/2015/05/19/16/16/minecraft-773807_1280.jpg",
+    "https://cdn.pixabay.com/photo/2016/08/08/10/48/minecraft-1578075_1280.png"
+]
