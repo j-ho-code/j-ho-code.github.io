83a5d58e1d466b1071cb3eb7fc748638908714d9
john <john@seanho.com>
2025-03-06 10:37:41 -0800

    Added more photos for Gallery page

--- gallery_script.js
+++ gallery_script.js
@@ -2,5 +2,6 @@ const imageGallery = [
     "https://cdn.pixabay.com/photo/2015/11/01/19/47/minecraft-1017471_1280.jpg",
     "https://cdn.pixabay.com/photo/2015/03/02/12/11/minecraft-655908_1280.png",
     "https://cdn.pixabay.com/photo/2015/05/19/16/16/minecraft-773807_1280.jpg",
-    "https://cdn.pixabay.com/photo/2016/08/08/10/48/minecraft-1578075_1280.png"
+    "https://cdn.pixabay.com/photo/2016/08/08/10/48/minecraft-1578075_1280.png",
+    "https://cdn.pixabay.com/photo/2016/08/30/07/52/minecraft-1629979_1280.png"
 ]
