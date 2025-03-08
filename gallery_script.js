b8ad28d2bc466309e37cd3232fe59726fadadd01
john <john@seanho.com>
2025-03-08 11:56:17 -0800

    fixed a few bugs and changed color of gallery button on hover.

--- gallery_script.js
+++ gallery_script.js
@@ -3,7 +3,8 @@ const imageGallery = [
     "https://cdn.pixabay.com/photo/2015/03/02/12/11/minecraft-655908_1280.png",
     "https://cdn.pixabay.com/photo/2015/05/19/16/16/minecraft-773807_1280.jpg",
     "https://cdn.pixabay.com/photo/2016/08/08/10/48/minecraft-1578075_1280.png",
-    "https://cdn.pixabay.com/photo/2016/08/30/07/52/minecraft-1629979_1280.png"
+    "https://cdn.pixabay.com/photo/2016/08/30/07/52/minecraft-1629979_1280.png",
+    "https://cdn.pixabay.com/photo/2016/08/08/10/42/minecraft-1578068_1280.png"
 ]
 
 const imageElement = document.getElementById("gallery-image");
